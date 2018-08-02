e380a2a81b134f5897b6f63f02dbfaa6e5eae283
OmarElhosseni <omarelhosseni123@gmail.com>
2018-08-02 14:39:45 -0400

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,32 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+let button = document.getElementsByClassName("button")[0];
+button.addEventListener('click', download);
+function download(event) {
+  window.open('https://itunes.apple.com/us/app/jukebox-music-with-friends/id1279561745', '_blank')
+}
